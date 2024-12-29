b74e992b6941d3b228181aaeb4a3db0a9c0a3e11
mswarankit <ankitnightr14@gmail.com>
2024-12-29 14:36:59 +0530

    add-feat: added logic for handleShorten

--- url_shortener/url_shortener.go
+++ url_shortener/url_shortener.go
@@ -1,6 +1,13 @@
 package main
 
-import "sync"
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+	"strings"
+	"sync"
+)
 
 type URLShortener struct {
 	urls     map[string]string
@@ -23,4 +30,47 @@ func NewURLShortener() *URLShortener {
 		stats:    make(map[string]int),
 		urlCache: make(map[string]string),
 	}
+}
+
+type MetricsResponse struct {
+	TopDomains []DomainStat `json:"top_domains"`
+}
+
+type DomainStat struct {
+	Domain string `json:"domain"`
+	Count  int    `json:"count"`
+}
+
+func (us *URLShortener) generateShortURL(url string) string {
+	hash := sha256.Sum256([]byte(url))
+	return base64.URLEncoding.EncodeToString(hash[:8])
+}
+
+func (us *URLShortener) extractDomain(url string) string {
+	parts := strings.Split(strings.TrimPrefix(url, "http://"), "/")
+	return strings.TrimPrefix(parts[0], "www.")
+}
+
+func (us *URLShortener) handleShorten(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req ShortenRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	us.mutex.Lock()
+	defer us.mutex.Unlock()
+
+	shortURL := us.generateShortURL(req.URL)
+	us.urls[shortURL] = req.URL
+	us.urlCache[req.URL] = shortURL
+
+	//updating my domain statistics
+	domain := us.extractDomain(req.URL)
+	us.stats[domain]++
+	json.NewEncoder(w).Encode(ShortenResponse{ShortURL: shortURL})
 }
